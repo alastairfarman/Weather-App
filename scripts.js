9551956e382099c57c451424e6ea40e6059eccaf
alastairfarman <106626671+alastairfarman@users.noreply.github.com>
2022-07-26 16:19:04 +0100

    changed to https

--- scripts.js
+++ scripts.js
@@ -1,48 +1,50 @@
-const nameElement = document.getElementById('name')
-const tempElement = document.getElementById('temp')
-const mainElement = document.getElementById('main')
-const descElement = document.getElementById('desc')
-const searchButton = document.getElementById('search-button')
-const searchBar = document.getElementById('search-bar')
-
+const nameElement = document.getElementById("name");
+const tempElement = document.getElementById("temp");
+const mainElement = document.getElementById("main");
+const descElement = document.getElementById("desc");
+const searchButton = document.getElementById("search-button");
+const searchBar = document.getElementById("search-bar");
 
 async function getWeather(city) {
-    const apiKey = "1b5148ce2b26ec158cbc87c179c39564"
-  const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q='
-    + city
-    + '&units='
-    +'metric' 
-    + '&APPID=' 
-    + apiKey,
-    {mode: 'cors'});
+  const apiKey = "1b5148ce2b26ec158cbc87c179c39564";
+  const response = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=" +
+      "metric" +
+      "&APPID=" +
+      apiKey,
+    { mode: "cors" }
+  );
   const data = await response.json();
-  renderWeather(data)
+  renderWeather(data);
 }
-getWeather("london")
+getWeather("london");
 
 function renderWeather(data) {
-    const { name } = data;
-    const { main, description } = data.weather[0];
-    const { temp } = data.main;
-    nameElement.innerText = name;
-    tempElement.innerText = Math.round(temp) + '°';
-    mainElement.innerText = main;
-    descElement.innerText = description[0].toUpperCase() + description.slice(1);
-    let imageSearchName = name.replace(/\s+/g, '');
-    document.body.style.backgroundImage = 'url(https://source.unsplash.com/1600x900?' + imageSearchName + ')';
-    console.log(name, main, description, temp);
+  const { name } = data;
+  const { main, description } = data.weather[0];
+  const { temp } = data.main;
+  nameElement.innerText = name;
+  tempElement.innerText = Math.round(temp) + "°";
+  mainElement.innerText = main;
+  descElement.innerText = description[0].toUpperCase() + description.slice(1);
+  let imageSearchName = name.replace(/\s+/g, "");
+  document.body.style.backgroundImage =
+    "url(https://source.unsplash.com/1600x900?" + imageSearchName + ")";
+  console.log(name, main, description, temp);
 }
 
 function search() {
-    getWeather(searchBar.value)
+  getWeather(searchBar.value);
 }
 
-searchButton.addEventListener('click', function() {
-    search()
-})
+searchButton.addEventListener("click", function () {
+  search();
+});
 
-searchBar.addEventListener('keyup', function(e) {
-    if (e.key =='Enter') {
-        search()
-    }})
-
+searchBar.addEventListener("keyup", function (e) {
+  if (e.key == "Enter") {
+    search();
+  }
+});
